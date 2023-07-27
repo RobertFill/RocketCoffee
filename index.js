3e2087d5630e27704a16571f8e6f7d052a9a32c1
Filipe Robert <filipe_soares@yahoo.com>
2023-07-27 14:06:31 -0300

    Rocketcoffe commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let menuMobile = document.querySelector(".mobile-container");
 let menuBtn = document.querySelector(".menu-btn");
 let content = document.querySelector(".content");
 
-menuBtn.addEventListener('click',clicar)
+menuBtn.addEventListener('click', clicar)
 function clicar(){
 
     if(!menuMobile.classList.contains('active')){
